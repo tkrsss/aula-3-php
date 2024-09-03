97827b2945d788149437ddf48f5271578edbe103
tkrsss <rian_dias@estudante.sesisenai.org.br>
2024-09-03 09:19:23 -0300

    delete and read feito com o professor

---
+++ delete.php
@@ -0,0 +1,19 @@
+<?php
+
+include 'db.php';
+
+$id = $_GET['id'];
+
+$sql = "DELETE FROM user WHERE id=$id";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Registro excluído com sucesso";
+} else {
+    echo "Erro: " . $sql . "<br>" . $conn->error;
+}
+
+$conn -> close();
+
+header ("Location: read.php");
+exit();
+?>
